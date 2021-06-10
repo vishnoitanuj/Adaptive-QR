e4df425813d338bbbf88987d08b44d7f8bb50ea6
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2021-06-10 23:16:46 +0530

    refactored app

--- app.py
+++ app.py
@@ -13,18 +13,13 @@ class SecureApi(Api):
         return url_for(self.endpoint('specs'), _external=True, _scheme=scheme)
 
 app = Flask(__name__)
-api = SecureApi(app)
 CORS(app, support_credentials=True)
 PORT = int(os.getenv('PORT', 8080))
 
-parser = reqparse.RequestParser()
-parser.add_argument('data', type=dict)
-parser.add_argument('index', type=str)
-
-@api.route('/makeQr', endpoint='with-parser')
-class QRMaker(Resource):
-    def post(self):
-        req_body = request.get_json(force=True)
+@app.route('/makeQr', methods = ['GET', 'POST'])
+def qrMaker():
+    if request.method == 'POST':
+        req_body = request.json
         qr_data = req_body['qr_data']
         index = req_body['index']
         try:
@@ -33,8 +28,6 @@ class QRMaker(Resource):
         except Exception as e:
             print("Error = ", str(e))
             return "Error Occured" 
-
-api.add_resource(QRMaker, '/makeQr')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=False, threaded=True)
