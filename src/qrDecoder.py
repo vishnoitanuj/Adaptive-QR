c1669fb7c4eb6c8752a3d87310236459cee99c5b
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2021-06-13 19:51:15 +0530

    decoder test commit

--- src/qrDecoder.py
+++ src/qrDecoder.py
@@ -1,18 +1,28 @@
 import cv2
 import numpy as np
 from pyzbar import pyzbar
+import json
 
 class Decoder:
 
     def cropQR(self, image):
         barcodes = pyzbar.decode(image)
-        cropped_qr = list()
+        cropped_qr_data = []
+        print(len(barcodes))
         for i, barcode in enumerate(barcodes):
-            (x,y,w,h) = barcode.rect
-            cropped_qr.append(image[y:y+h, x+x:w])
-        return cropped_qr
+            (x, y, w, h) = barcode.rect
+            # crop_image = image[y:y+h, x:x+h]
+            # cv2.imwrite(str(i)+'.jpg',crop_image)
+            # cropped_qr.append(crop_image)
+            barcodeData = barcode.data.decode("utf-8")
+            print(barcodeData)
+            return barcodeData
+            # barcodeType = barcode.type
+            cropped_qr_data.append(barcodeData)
+            # print(barcodeData, barcodeType)
+        return cropped_qr_data
     
-    def automatic_brightness_and_contrast(image, clip_hist_percent=25):
+    def automatic_brightness_and_contrast(self, image, clip_hist_percent=15):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         hist = cv2.calcHist([gray],[0],None,[256],[0,256])
@@ -44,3 +54,24 @@ class Decoder:
 
         auto_result = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
         return (auto_result, alpha, beta)
+
+    @staticmethod
+    def base64_to_image(base64Image):
+        imgdata = base64.b64decode(base64Image)
+        image = cv2.imread(imgdata)
+        return image
+
+if __name__ == '__main__':
+    image = cv2.imread('test.jpeg')
+    decode = Decoder()
+    im, _, _ = decode.automatic_brightness_and_contrast(image)
+    # cv2.imwrite('cleared.jpg', im)
+    images = eval(decode.cropQR(im))
+    print(type(images))
+    # # print(len(images))
+    # for i, image in enumerate(images):
+    #     if image is None:
+    #         print("none")
+    #     else:
+    #         cv2.imwrite(str(i)+'.jpg', image)
+    
